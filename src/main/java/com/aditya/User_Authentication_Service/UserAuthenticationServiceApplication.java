7314b1924424c2c1a174f5fb66342c329862b738
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 14:52:38 +0530

    Added jwtFilter in Runable class

--- src/main/java/com/aditya/User_Authentication_Service/UserAuthenticationServiceApplication.java
+++ src/main/java/com/aditya/User_Authentication_Service/UserAuthenticationServiceApplication.java
@@ -1,13 +1,24 @@
 package com.aditya.User_Authentication_Service;
 
+import com.aditya.User_Authentication_Service.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserAuthenticationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserAuthenticationServiceApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+		FilterRegistrationBean<JwtFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new JwtFilter());
+		filterRegistrationBean.addUrlPatterns("/api/v1/*");
+		return filterRegistrationBean;
 	}
 
 }
