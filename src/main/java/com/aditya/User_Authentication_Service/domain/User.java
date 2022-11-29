c315e0f12b4b3dcba1586edfc4422325d1951e46
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:39:31 +0530

    generated getter and setter

--- src/main/java/com/aditya/User_Authentication_Service/domain/User.java
+++ src/main/java/com/aditya/User_Authentication_Service/domain/User.java
@@ -26,4 +26,52 @@ public class User {
 		this.password = password;
 		this.mobile = mobile;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 }
