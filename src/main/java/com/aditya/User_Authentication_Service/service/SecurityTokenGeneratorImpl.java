cb0cff17e2132c46a766adec25ff64581e162005
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 14:19:40 +0530

    added generateToken method

--- src/main/java/com/aditya/User_Authentication_Service/service/SecurityTokenGeneratorImpl.java
+++ src/main/java/com/aditya/User_Authentication_Service/service/SecurityTokenGeneratorImpl.java
@@ -8,14 +8,27 @@
 package com.aditya.User_Authentication_Service.service;
 
 import com.aditya.User_Authentication_Service.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class SecurityTokenGeneratorImpl implements SecurityTokenGenerator {
 	@Override
 	public Map<String, String> generateToken(User user) {
-		return null;
+		String jwtToken;
+		jwtToken = Jwts.builder().setSubject(user.getUserName()) // Header
+			.setIssuedAt(new Date()) // Payload
+			.signWith(SignatureAlgorithm.HS256, "secretkey.") // Verify Signature
+			.compact();
+
+		Map<String, String> map = new HashMap<>();
+		map.put("token", jwtToken);
+		map.put("message", "User Successfully logged in");
+		return map;
 	}
 }
