f9d56c5b65192adefe0fa328ee1ec3b138135225
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:49:07 +0530

    created UserService interface

--- src/main/java/com/aditya/User_Authentication_Service/service/UserService.java
+++ src/main/java/com/aditya/User_Authentication_Service/service/UserService.java
@@ -1,4 +1,14 @@
 package com.aditya.User_Authentication_Service.service;
 
+import com.aditya.User_Authentication_Service.domain.User;
+
+import java.util.List;
+
+
 public interface UserService {
+	User saveUser(User user);
+	List<User> getAllUser();
+	User getByUserId(int id);
+	User findByUserNameAndPassword(String username, String password);
+
 }
