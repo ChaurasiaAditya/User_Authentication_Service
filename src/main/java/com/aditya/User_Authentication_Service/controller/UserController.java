72e38359100e61d9ed23a4ab0c052db3bf99217a
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:58:09 +0530

    created method for savemethod

--- src/main/java/com/aditya/User_Authentication_Service/controller/UserController.java
+++ src/main/java/com/aditya/User_Authentication_Service/controller/UserController.java
@@ -7,5 +7,30 @@
  */
 package com.aditya.User_Authentication_Service.controller;
 
+import com.aditya.User_Authentication_Service.domain.User;
+import com.aditya.User_Authentication_Service.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1")
 public class UserController {
+
+	private final UserServiceImpl userService;
+
+	@Autowired
+	public UserController(UserServiceImpl userService) {
+		this.userService = userService;
+	}
+
+	@PostMapping("add")
+	public ResponseEntity<?> addUser(User user) {
+		return new ResponseEntity<>(this.userService.saveUser(user), HttpStatus.CREATED);
+	}
 }
