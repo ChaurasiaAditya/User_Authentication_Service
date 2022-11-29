79f206680fe1b0e84d15bcb1200d98fbccceb1a1
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:50:44 +0530

    created constructor

--- src/main/java/com/aditya/User_Authentication_Service/service/UserServiceImpl.java
+++ src/main/java/com/aditya/User_Authentication_Service/service/UserServiceImpl.java
@@ -8,12 +8,22 @@
 package com.aditya.User_Authentication_Service.service;
 
 import com.aditya.User_Authentication_Service.domain.User;
+import com.aditya.User_Authentication_Service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	@Override
 	public User saveUser(User user) {
 		return null;
