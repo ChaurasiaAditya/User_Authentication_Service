b520f721ba4c7c318085ba256c87d51b200ac3b8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 10:06:01 +0530

    added Response Status

--- src/main/java/com/aditya/User_Authentication_Service/exception/UserNotFoundException.java
+++ src/main/java/com/aditya/User_Authentication_Service/exception/UserNotFoundException.java
@@ -7,5 +7,10 @@
  */
 package com.aditya.User_Authentication_Service.exception;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid Username and Password")
+public class UserNotFoundException extends Exception{
+
 }
