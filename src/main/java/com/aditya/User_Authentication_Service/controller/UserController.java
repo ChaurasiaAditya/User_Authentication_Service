22855dd122a8b4ed8840ce62e5790c871232038e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:55:34 +0530

    created UserController class

---
+++ src/main/java/com/aditya/User_Authentication_Service/controller/UserController.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 29-11-2022
+ * Created With: IntelliJ IDEA Ultimate
+ * Profile: github.com/ChaurasiaAditya
+ * Website: ChaurasiaAditya.in
+ */
+package com.aditya.User_Authentication_Service.controller;
+
+public class UserController {
+}
