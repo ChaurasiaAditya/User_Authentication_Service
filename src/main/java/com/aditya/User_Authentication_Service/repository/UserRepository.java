aaf6153fb487be0eb6d32b32637954bd7c50669f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-29 09:48:22 +0530

    complited repository

---
+++ src/main/java/com/aditya/User_Authentication_Service/repository/UserRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 29-11-2022
+ * Created With: IntelliJ IDEA Ultimate
+ * Profile: github.com/ChaurasiaAditya
+ * Website: ChaurasiaAditya.in
+ */
+package com.aditya.User_Authentication_Service.repository;
+
+import com.aditya.User_Authentication_Service.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	User findByUserNameAndPassword(String username, String password);
+
+}
